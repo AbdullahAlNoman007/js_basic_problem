80a5387d45e98193008bf193b78265436c90055b
Sunan13208 <sunanrabbi1918@gmail.com>
2023-01-10 17:58:46 +0600

    hii

---
+++ slice.js
@@ -0,0 +1,3 @@
+const array1= [2,3,44,5,1,5,6,66,79]
+
+console.log(array1.slice(2,5))
---
+++ slipce.js
@@ -0,0 +1,5 @@
+const array1= [2,3,44,5,1,5,6,66,79]
+
+const new_array=array1.splice(2,1)
+console.log(array1)
+console.log(new_array)
